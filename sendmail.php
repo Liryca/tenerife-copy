5879d697513646013d0a417f78be65ffd8f5e312
Tarasova Anna <tarasavahanna@gmail.com>
2023-03-21 14:22:59 +0300

    first commit

---
+++ sendmail.php
@@ -0,0 +1,54 @@
+ <?php 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('taasuper@gmail.com');
+	//Кому отправить
+	$mail->addAddress('taasuper@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Получить программу путешествия';
+	//Тело письма
+	$body = '<h1>Письмо с сайта Tenerife</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Возраст:</strong> '.$_POST['import'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['export'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['item'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['amount'].'</p>';
+	}
+
+
+	$mail->Body = $body;
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+	$response = ['message' => $message];
+	header('Content-type: application/json');
+	echo json_encode($response);
+ ?>
