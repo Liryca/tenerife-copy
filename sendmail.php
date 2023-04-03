9b19835fdd5c9e47a83b8ee2405e49d2423a916f
Tarasova Anna <tarasavahanna@gmail.com>
2023-04-03 13:01:43 +0300

    commit

--- sendmail.php
+++ sendmail.php
@@ -1,54 +1,115 @@
- <?php 
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
+ <?php
+ 
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
 
-	require 'phpmailer/src/Exception.php';
-	require 'phpmailer/src/PHPMailer.php';
+if (!error_get_last()) {
 
-	$mail = new PHPMailer(true);
-	$mail->CharSet = 'UTF-8';
-	$mail->setLanguage('ru', 'phpmailer/language/');
-	$mail->IsHTML(true);
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'] ;
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+ 
+    // Формирование самого письма
+    $title = "Получить программу путешествия";
+    $body = "
+    <h1>Письмо с сайта Tenerife</h1>
+	<h2>Контактные данные</h2>
+    <b>Имя:</b> $name<br>
+    <b>Почта:</b> $email<br><br>
+	<b>Phone:</b> $phone<br><br>
+    ";
+    
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};
+    
+    // Настройки вашей почты
 
-	//От кого письмо
-	$mail->setFrom('taasuper@gmail.com');
-	//Кому отправить
-	$mail->addAddress('taasuper@gmail.com');
-	//Тема письма
-	$mail->Subject = 'Получить программу путешествия';
-	//Тело письма
-	$body = '<h1>Письмо с сайта Tenerife</h1>';
-
-	if(trim(!empty($_POST['name']))){
-		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-	}
-	if(trim(!empty($_POST['email']))){
-		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-	}
-
-	if(trim(!empty($_POST['age']))){
-		$body.='<p><strong>Возраст:</strong> '.$_POST['import'].'</p>';
-	}
-	
-	if(trim(!empty($_POST['message']))){
-		$body.='<p><strong>Сообщение:</strong> '.$_POST['export'].'</p>';
-	}
-	if(trim(!empty($_POST['message']))){
-		$body.='<p><strong>Сообщение:</strong> '.$_POST['item'].'</p>';
-	}
-	if(trim(!empty($_POST['message']))){
-		$body.='<p><strong>Сообщение:</strong> '.$_POST['amount'].'</p>';
-	}
+    $mail->Host = 'smtp.gmail.com'; // SMTP сервер
+	$mail->SMTPSecure = 'ssl'; // шифрование
+	$mail->Port = 465; // Порт
+    $mail->Username   = 'nikonorova6666'; // Логин на почте
+    $mail->Password   = 'dudbhkxmhsncgpyf'; // Пароль на почте
+    $mail->setFrom('nikonorova6666@gmail.com', 'Anna'); // Адрес самой почты и имя отправителя
+    
+    // Получатель письма
+    $mail->addAddress('agalunia71@gmail.com'); 
 
 
-	$mail->Body = $body;
-	//Отправляем
-	if (!$mail->send()) {
-		$message = 'Ошибка';
-	} else {
-		$message = 'Данные отправлены!';
-	}
-	$response = ['message' => $message];
-	header('Content-type: application/json');
-	echo json_encode($response);
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+    
+    // Проверяем отправленность сообщения
+    if ($mail->send()){
+        $data['result'] = "success";
+        $data['info'] = "Сообщение успешно отправлено!";
+    } else {
+        $data['result'] = "error";
+        $data['info'] = "Сообщение не было отправлено. Ошибка при отправке письма";
+        $data['desc'] = "Причина ошибки: {$mail->ErrorInfo}";
+    }
+    
+} else {
+    $data['result'] = "error";
+    $data['info'] = "В коде присутствует ошибка";
+    $data['desc'] = error_get_last();
+}
+
+// Отправка результата
+header('Content-Type: application/json');
+echo json_encode($data);
+
+
+
+
+	// $mail = new PHPMailer(true);
+	// $mail->CharSet = 'UTF-8';
+	// $mail->setLanguage('ru', 'phpmailer/language/');
+	// $mail->IsHTML(true);
+	// $mail->SMTPDebug = 2;
+
+    // $mail->Host = ‘smtp.gmail.com’; // SMTP сервер
+	// $mail->SMTPSecure = ‘ssl’; // шифрование
+	// $mail->Port = 465; // Порт
+    // $mail->Username   = 'nikonorova6666'; // Логин на почте
+    // $mail->Password   = 'dudbhkxmhsncgpyf'; // Пароль на почте
+    // $mail->setFrom('nikonorova6666@gmail.com', 'Anna'); // Адрес самой почты и имя отправителя
+	// $mail->addAddress('tarasavahanna@gmail.com')
+
+
+    // $mail->Subject = 'Получить программу путешествия';
+    // $mail->Body = '<h1>Письмо с сайта Tenerife</h1>';  
+
+
+
+	// if(trim(!empty($_POST['name']))){
+	// 	$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	// }
+	// if(trim(!empty($_POST['phone']))){
+	// 	$body.='<p><strong>Email:</strong> '.$_POST['import'].'</p>';
+	// }
+
+	// if(trim(!empty($_POST['email']))){
+	// 	$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	// }
+
+	// $mail->Body = $body;
+	// //Отправляем
+	// if (!$mail->send()) {
+	// 	$message = 'Ошибка';
+	// } else {
+	// 	$message = 'Данные отправлены!';
+	// }
+	// $response = ['message' => $message];
+	// header('Content-type: application/json');
+	// echo json_encode($response);
  ?>
